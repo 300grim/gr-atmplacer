4eb3777d174a7cdece1d0f45fe8b3a708c74c6f4
grim <136266513+300grim@users.noreply.github.com>
2024-12-03 12:37:42 -0600

    remove unused notes

--- gr-atmplacer/client.lua
+++ gr-atmplacer/client.lua
@@ -1,10 +1,10 @@
 local QBCore = exports['qb-core']:GetCoreObject()
 
--- Spawning of ATM models
+
 local function SpawnATM(location)
     local model = location.model
     local x, y, z = location.x, location.y, location.z
-    local heading = location.heading  -- Get the heading from the location
+    local heading = location.heading  
 
     RequestModel(model)
     while not HasModelLoaded(model) do
@@ -12,26 +12,26 @@ local function SpawnATM(location)
     end
 
     local atmObject = CreateObject(model, x, y, z, false, true, true)
-    SetEntityHeading(atmObject, heading)  -- Use the heading from the location
+    SetEntityHeading(atmObject, heading)  
     FreezeEntityPosition(atmObject, true)
     SetEntityAsMissionEntity(atmObject, true, true)
 end
 
--- Load all ATMs from the config
+
 local function LoadATMs()
     for _, atm in pairs(Config.ATMData) do
         SpawnATM(atm)
     end
 end
 
--- Load ATMs when the resource starts
+
 AddEventHandler('onResourceStart', function(resourceName)
     if resourceName == GetCurrentResourceName() then
         LoadATMs()
     end
 end)
 
--- Optionally load ATMs if you want to refresh them
+
 AddEventHandler('playerSpawned', function()
     LoadATMs()
 end)
